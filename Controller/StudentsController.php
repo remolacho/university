bf050369de89e6a1a3e3f68ae2484b050c7ab0fa
Jonathan <jonathan.rojas@globalr.net>
2015-07-30 14:41:35 -0430

    it was added the table for the students in the index.cpt file

--- Controller/StudentsController.php
+++ Controller/StudentsController.php
@@ -9,15 +9,15 @@ class StudentsController extends AppController {
 	}
 
     public function add(){
-	  if($this->request->is('post')){
-	  	if($this->Student->save($this->request->data)){
+	  if($this->request->is('post')):
+	  	if($this->Student->save($this->request->data)):
           $this->Session->setFlash("It was recorded the student with success");
 	  	  $this->redirect(array('action' => 'index'));
-	  	}else{
+	  	else:
           $this->Session->setFlash("It wasn't recorded the student");
 	  	  $this->redirect(array('action' => 'index'));
-	  	}
-	  }
+	  	endif;
+	  endif;
 	}
 
 }
--- View/Students/index.ctp
+++ View/Students/index.ctp
@@ -1,6 +1,19 @@
 <h1>students' list</h1>
 <pre>
-<?php
-  print_r(array($students));
-?>
+<table>
+	<tbody>
+		<th>Id</th>
+		<th>Name</th>
+		<th>Last Name</th>
+		<th>Address</th>
+		<?php foreach ($students as $student):?>
+      	  <tr>
+	      	  <th><?php echo $student['Student']['id']; ?></th>
+			  <th><?php echo $student['Student']['name']; ?></th>
+			  <th><?php echo $student['Student']['last_name']; ?></th>
+			  <th><?php echo $student['Student']['address']; ?></th>
+	      </tr>
+	    <?php endforeach;?>
+	</tbody>
+</table>
 </pre>
